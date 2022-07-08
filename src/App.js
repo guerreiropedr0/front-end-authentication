9c5daa65b2144f0e6f68aca9c69fb580d85a8fba
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-07-08 11:23:59 +0100

    Add basic router configuration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App" />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element="" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

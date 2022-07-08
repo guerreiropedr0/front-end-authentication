7472e1e256210a1031902fb755a7fb7ab8056137
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-07-08 11:52:43 +0100

    Add controlled inputs to LoginForm

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,17 +1,35 @@
-const LoginForm = () => (
+import { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   /* eslint-disable jsx-a11y/label-has-associated-control */
-  <form>
-    <div className="mb-3">
-      <label htmlFor="username" className="form-label">Email address</label>
-      <input type="text" className="form-control" id="username" />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="password" className="form-label">Password</label>
-      <input type="password" className="form-control" id="password" />
-    </div>
-    <button type="submit" className="btn btn-primary">Submit</button>
-  </form>
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="username" className="form-label">Email address</label>
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="password" className="form-label">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  );
   /* eslint-enable jsx-a11y/label-has-associated-control */
-);
+};
 
 export default LoginForm;
